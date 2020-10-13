79599667a4cebe759483ab82a5d9912a99a0db75
jpnavarro1 <juanpablonavarro04@gmail.com>
2020-10-13 14:33:12 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+	const btnMas = document.querySelector('.mas')
+	const btnMenos = document.querySelector('.menos')
+	const body = document.querySelector('body')
+	const top = document.querySelector('.top')
+	const bottom = document.querySelector('.bottom')
+	const mesa = document.querySelector('.mesa')
+
+	let numeroSilla = 0
+
+	btnMas.onclick = function() {
+		const nuevaSilla = document.createElement('img')
+		nuevaSilla.src = './material/silla.png'
+		let clasesSilla = ''
+		switch(numeroSilla) {
+			case 0: 
+				clasesSilla += 'primera '
+			case 1:
+			case 2:
+				clasesSilla += 'arriba'
+			break
+			case 3:
+				clasesSilla = 'lateral izquierda'
+			break
+			case 4:
+				clasesSilla = 'lateral derecha'
+			break
+			case 5:
+				clasesSilla += 'primera '
+			case 6:
+			case 7:
+				clasesSilla += 'abajo'
+
+		}
+
+		if(clasesSilla != '') {
+			nuevaSilla.className = clasesSilla + ' silla'
+			switch(numeroSilla) {
+				case 0:
+				case 1:
+				case 2:
+					body.insertBefore(nuevaSilla, top)
+				break
+				case 3:
+					body.insertBefore(nuevaSilla, mesa)
+				break
+				case 4:
+					body.insertBefore(nuevaSilla, bottom)
+				break
+				default:
+					body.append(nuevaSilla)
+			}
+			numeroSilla++
+		}
+	}
+
+	btnMenos.onclick = function() {
+		if(numeroSilla > 0) {
+			ultimaSilla = document.querySelectorAll('.silla')
+			ultimaSilla = ultimaSilla[ultimaSilla.length - 1]
+			body.removeChild(ultimaSilla)
+			numeroSilla--
+		}
+	}
+}
